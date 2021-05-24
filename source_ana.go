2530ad4fa906ce50e66944e3af029ad2f922034c
michaelpei <michaelpei@tencent.com>
2021-05-24 10:49:08 +0800

    add expvar relative comment

--- source_ana.go
+++ source_ana.go
@@ -5,13 +5,30 @@
    @Email: xingxinpei@gmail, michaelpei@tencent.com
    @Description:
 */
-package mic_test
+package main
 
 import (
 	"expvar"
 	"net/http"
+	"time"
 )
 
 func main() {
-	http.ListenAndServe(":8080", expvar.Handler())
+	lastAccess := expvar.NewString("lastAccess")
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("hello"))
+		lastAccess.Set(time.Now().String())
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
+
+// http :8080/debug/vars | grep lastAccess
+// 在 expvar.go 中有init，注册了debug/vars，会发布所有Publish的变量
+/*
+func init() {
+        http.HandleFunc("/debug/vars", expvarHandler)
+        Publish("cmdline", Func(cmdline))
+        Publish("memstats", Func(memstats))
+}
+ */
