42b08a50a56ca40b1ab70e60364f9764c28f4e17
michaelpei <michaelpei@tencent.com>
2021-05-28 00:06:18 +0800

    union_find ut finsihed

--- algorithm/union_find_test.go
+++ algorithm/union_find_test.go
@@ -47,3 +47,54 @@ func TestUnionFind_IsConnected(t *testing.T) {
 	a.False(v.IsConnected(2, 3))
 	a.False(v.IsConnected(2, 4))
 }
+
+func TestUnionFind_Find(t *testing.T) {
+	v := NewUnionFind(5)
+	a := assert.New(t)
+
+	// {0->1}, 2, 3, 4
+	v.Union(0, 1)
+
+	// {0, 1}, 2, {3->4}
+	v.Union(3, 4)
+
+	// {0->1->2}, {3->4}
+	v.Union(2, 1)
+
+	a.Equal(1, v.Find(1))
+	a.Equal(1, v.Find(0))
+	a.Equal(1, v.Find(2))
+	a.Equal(4, v.Find(4))
+	a.Equal(4, v.Find(3))
+
+	// {0->1->2->3->4}
+	// In fact, after compaction
+	// {0, 1, 2, 3} -> {4}
+	v.Union(0, 3)
+	a.Equal(4, v.Find(0))
+}
+
+func TestUnionFind_Union(t *testing.T) {
+	v := NewUnionFind(5)
+	a := assert.New(t)
+
+	// {0, 1}, 2, 3, 4
+	v.Union(0, 1)
+	a.Equal(4, v.Count())
+
+	// {0, 1}, 2, {3, 4}
+	v.Union(3, 4)
+	a.Equal(3, v.Count())
+
+	// {0, 1, 2}, {3, 4}
+	v.Union(2, 1)
+	a.Equal(2, v.Count())
+
+	// Repeat Union nothing change
+	v.Union(2, 1)
+	a.Equal(2, v.Count())
+
+	// {0, 1, 2, 3, 4, 5}
+	v.Union(0, 4)
+	a.Equal(1, v.Count())
+}
