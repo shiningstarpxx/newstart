8d6d5f7ef7dffd47c1d87b62510976e9c8768a54
michaelpei <michaelpei@tencent.com>
2021-05-27 23:51:33 +0800

    finish first ut for union-find

--- algorithm/union_find_test.go
+++ algorithm/union_find_test.go
@@ -8,13 +8,17 @@
 package algorithm
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestNewUnionFind(t *testing.T) {
-
+	a := assert.New(t)
 	v := NewUnionFind(5)
-	if v.Count() != 5 {
-		t.Errorf("Unexpect")
-	}
+	a.Equal(5, v.Count())
+
+	// {0, 1}, 2, 3, 4
+	v.Union(0, 1)
+	a.Equal(4, v.Count())
 }
+
