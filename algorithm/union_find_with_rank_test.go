d0bcb9e9e49f7ce6b0a0720a41a0eb3cefefc53e
michaelpei <michaelpei@tencent.com>
2021-05-30 11:22:51 +0800

    ut show two kinds of union for disjoint set, rank can control the height of tree

--- algorithm/union_find_with_rank_test.go
+++ algorithm/union_find_with_rank_test.go
@@ -75,39 +75,59 @@ func TestUnionFindWithRank_Find(t *testing.T) {
 }
 
 func TestUnionFindWithRank_Union(t *testing.T) {
-	v := NewUnionFindWithRank(5)
+	v := NewUnionFind(5)
+	v1 := NewUnionFindWithRank(5)
 	a := assert.New(t)
 
-	// {0, 1}, 2, 3, 4
+	// {0, 1}->2, 2->1, 3->1, 4->1
 	v.Union(0, 1)
+	v1.Union(0, 1)
 	a.Equal(4, v.Count())
 	// TODO(michael), Need find a way to test lacal value
 	a.Equal(1, v.Find(0))
 	a.Equal(1, v.Find(1))
+	a.Equal(1, v1.Find(0))
+	a.Equal(1, v1.Find(0))
 
-	// {0, 1}, 2, {3, 4}
+
+	// {0, 1}->2, 2->1, {3, 4}->2
 	v.Union(3, 4)
+	v1.Union(3, 4)
 	a.Equal(3, v.Count())
 	a.Equal(4, v.Find(3))
 	a.Equal(4, v.Find(4))
+	a.Equal(4, v1.Find(3))
+	a.Equal(4, v1.Find(4))
 
-	// {0, 1, 2}, {3, 4}
-	v.Union(2, 1)
-	a.Equal(2, v.Count())
-	a.Equal(1, v.Find(0))
-	a.Equal(1, v.Find(1))
-	a.Equal(1, v.Find(2))
-
-	// Repeat Union nothing change
-	v.Union(2, 1)
-	a.Equal(2, v.Count())
-
-	// {0, 1, 2, 3, 4}
+	// {{0, 1, 3, 4}->3, 2->1}
 	v.Union(0, 4)
-	a.Equal(1, v.Count())
+	v1.Union(0, 4)
+	a.Equal(2, v.Count())
+	a.Equal(2, v1.Count())
 	a.Equal(4, v.Find(0))
 	a.Equal(4, v.Find(1))
-	a.Equal(4, v.Find(2))
 	a.Equal(4, v.Find(3))
 	a.Equal(4, v.Find(4))
+	a.Equal(4, v1.Find(0))
+	a.Equal(4, v1.Find(1))
+	a.Equal(4, v1.Find(3))
+	a.Equal(4, v1.Find(4))
+
+	// {0, 1, 2, 3, 4} join big to small size
+	// {0, 1, 2, 3, 4}->3
+	v.Union(0, 2)
+	v1.Union(0, 2)
+	a.Equal(1, v1.Count())
+	a.Equal(1, v1.Count())
+	a.Equal(2, v.Find(0))
+	a.Equal(2, v.Find(1))
+	a.Equal(2, v.Find(2))
+	a.Equal(2, v.Find(3))
+	a.Equal(2, v.Find(4))
+	a.Equal(4, v1.Find(0))
+	a.Equal(4, v1.Find(1))
+	a.Equal(4, v1.Find(2))
+	a.Equal(4, v1.Find(3))
+	a.Equal(4, v1.Find(4))
+
 }
