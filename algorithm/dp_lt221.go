dd654d00dd0ac884ee03981ac690150d36b8f468
michaelpei <michaelpei@tencent.com>
2021-06-24 22:56:58 +0800

    dp lt221 passed

---
+++ algorithm/dp_lt221.go
@@ -0,0 +1,38 @@
+/*
+   @Copyright: Tencent (1998-2022)
+   @Author: Pei, Xingxin
+   @Created:  10:34 PM  24/6/2021
+   @Email: xingxinpei@gmail, michaelpei@tencent.com
+   @Description:
+*/
+package algorithm
+
+import "math"
+
+func maximalSquare(matrix [][]byte) int {
+	m := len(matrix)
+	if m == 0 {
+		return 0
+	}
+	n := len(matrix[0])
+
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+
+	res := 0
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			dp[i][j] = int(matrix[i][j] - '0')
+			if dp[i][j] == 0 {
+				continue;
+			}
+			if i != 0 && j != 0 {
+				dp[i][j] = int(math.Min(float64(dp[i-1][j-1]), math.Min(float64(dp[i-1][j]), float64(dp[i][j-1])))) + 1
+			}
+			res = int(math.Max(float64(res), float64(dp[i][j] * dp[i][j])))
+		}
+	}
+	return res
+}
---
+++ algorithm/dp_lt221_test.go
@@ -0,0 +1,43 @@
+/*
+   @Copyright: Tencent (1998-2022)
+   @Author: Pei, Xingxin
+   @Created:  10:45 PM  24/6/2021
+   @Email: xingxinpei@gmail, michaelpei@tencent.com
+   @Description:
+*/
+package algorithm
+
+import "testing"
+
+// [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+func Test_maximalSquare(t *testing.T) {
+	type args struct {
+		matrix [][]byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name : "simple test",
+			args: args{
+				matrix: [][]byte {
+					{'1', '0', '1', '0', '0'},
+					{'1', '0', '1', '1', '1'},
+					{'1', '1', '1', '1', '1'},
+					{'1', '0', '0', '1', '0'},
+				},
+			},
+			want : 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maximalSquare(tt.args.matrix); got != tt.want {
+				t.Errorf("maximalSquare() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
