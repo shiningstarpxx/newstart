e561bb3f8944228ee4dbc533c2f0dd4164d65416
michaelpei <michaelpei@tencent.com>
2021-06-25 22:37:55 +0800

    recursive lt22 passed

---
+++ algorithm/recursive_lt22.go
@@ -0,0 +1,28 @@
+/*
+   @Copyright: Tencent (1998-2022)
+   @Author: Pei, Xingxin
+   @Created:  10:18 PM  25/6/2021
+   @Email: xingxinpei@gmail, michaelpei@tencent.com
+   @Description:
+*/
+package algorithm
+
+func generateParenthesis(n int) []string {
+	res := helper(n, n, "")
+	return res
+}
+
+func helper(left, right int, s string) []string {
+	var res []string
+	if left == 0 && right == 0 {
+		res = append(res, s)
+		return res
+	}
+	if left > 0 {
+		res = append(res, helper(left - 1, right, s + "(")...)
+	}
+	if right > left {
+		res = append(res, helper(left, right - 1, s + ")")...)
+	}
+	return res
+}
---
+++ algorithm/recursive_lt22_test.go
@@ -0,0 +1,59 @@
+/*
+   @Copyright: Tencent (1998-2022)
+   @Author: Pei, Xingxin
+   @Created:  10:28 PM  25/6/2021
+   @Email: xingxinpei@gmail, michaelpei@tencent.com
+   @Description:
+*/
+package algorithm
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_generateParenthesis(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		// TODO: Add test cases.
+		{
+			name : "simple test",
+			args: args{
+				n : 3,
+			},
+			want: []string{"((()))", "(()())", "(())()", "()(())", "()()()"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := generateParenthesis(tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("generateParenthesis() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_helper(t *testing.T) {
+	type args struct {
+		left  int
+		right int
+		s     string
+		res   []string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+		})
+	}
+}
