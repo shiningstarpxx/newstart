6a867c707d3d3ca6cd01ce6799cf33c4c533fcb5
裴星鑫 <michaelpei@tencent.com>
2021-08-12 23:21:22 +0800

    dp lt516, passed

--- algorithm/leetcode/dp_lt516.go
+++ algorithm/leetcode/dp_lt516.go
@@ -34,3 +34,33 @@ func max(a, b int) int {
 	}
 	return b
 }
+
+
+
+/*  官方答案, 也说明了，dp要想清楚，解法不唯一
+func longestPalindromeSubseq(s string) int {
+    n := len(s)
+    dp := make([][]int, n)
+    for i := range dp {
+        dp[i] = make([]int, n)
+    }
+    for i := n - 1; i >= 0; i-- {
+        dp[i][i] = 1
+        for j := i + 1; j < n; j++ {
+            if s[i] == s[j] {
+                dp[i][j] = dp[i+1][j-1] + 2
+            } else {
+                dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+            }
+        }
+    }
+    return dp[0][n-1]
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+*/
