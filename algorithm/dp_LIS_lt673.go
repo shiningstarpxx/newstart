a581962a7ac6fdd703cc1a2faf52e246679e838a
裴星鑫 <michaelpei@tencent.com>
2021-09-20 17:52:11 +0800

    dp LIS, lt672, passed

---
+++ algorithm/dp_LIS_lt673.go
@@ -0,0 +1,44 @@
+/**
+ * @Author: pei, xingxin
+ * @mail: xingxinpei@gmail.com
+ * @Description:
+ * @File: dp_LIS_lt673.go
+ * @Version: 1.0.0
+ * @Data: 2021/9/20 5:35 PM
+ */
+package algorithm
+
+func findNumberOfLIS(nums []int) int {
+	n := len(nums)
+
+	dp := make([]int, n + 1)  // 长度
+	cnt := make([]int, n + 1) // 长度下的count
+	maxLen := 0
+
+	for i := 0; i < n; i++ {
+		dp[i] = 1
+		cnt[i] = 1
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] {
+				if dp[j] + 1 > dp[i] {
+					dp[i] = dp[j] + 1
+					cnt[i] = cnt[j]
+				} else if dp[j] + 1 == dp[i] {
+					cnt[i] += cnt[j]
+				}
+			}
+		}
+
+		if dp[i] > maxLen {
+			maxLen = dp[i]
+		}
+	}
+
+	ans := 0
+	for i := 0; i < n; i++ {
+		if dp[i] == maxLen {
+			ans += cnt[i]
+		}
+	}
+	return ans
+}
---
+++ algorithm/dp_LIS_lt673_test.go
@@ -0,0 +1,44 @@
+/**
+ * @Author: pei, xingxin
+ * @mail: xingxinpei@gmail.com
+ * @Description:
+ * @File: dp_LIS_lt673_test.go
+ * @Version: 1.0.0
+ * @Data: 2021/9/20 5:45 PM
+ */
+package algorithm
+
+import "testing"
+
+func Test_findNumberOfLIS(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"simple test",
+			args{
+				[]int{1, 3, 5, 4, 7},
+			},
+			2,
+		},
+		{
+			"special case",
+			args{
+				[]int{2, 2, 2, 2, 2},
+			},
+			5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findNumberOfLIS(tt.args.nums); got != tt.want {
+				t.Errorf("findNumberOfLIS() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
