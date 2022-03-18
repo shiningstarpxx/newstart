9366b08288420995273e63d7d1067561747e8052
michaelpei <michaelpei@tencent.com>
2022-03-18 22:16:21 +0800

    vector sort 692, passed

---
+++ algorithm/leetcode/cpp/vector_sort_692.cpp
@@ -0,0 +1,28 @@
+//
+// Created by 裴星鑫 on 2022/3/18.
+//
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<string> topKFrequent(vector<string>& words, int k) {
+    unordered_map<string, int> cache_;
+    for (auto& s : words) cache_[s]++;
+
+    vector<string> res;
+    vector<pair<string, int>> data;
+    for(auto [k, v] : cache_) data.push_back({k, v});
+
+    auto cmp = [](pair<string, int>& a, pair<string, int>& b) {
+      if (a.second == b.second) return a.first < b.first;
+      return a.second > b.second;
+    };
+    sort(data.begin(), data.end(), cmp);
+
+    for (int i = 0; i < k; i++) res.push_back(data[i].first);
+    return res;
+  }
+};
