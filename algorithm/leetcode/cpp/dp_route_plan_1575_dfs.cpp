aa205b7a815bf8f47a7bc1ccef7622f8e57aab58
michaelpei <michaelpei@tencent.com>
2022-03-26 14:37:27 +0800

    dp route plan 1575 with memorized dfs and plan dp, passed

--- algorithm/leetcode/cpp/dp_route_plan_1575_dfs.cpp
+++ algorithm/leetcode/cpp/dp_route_plan_1575_dfs.cpp
@@ -10,15 +10,18 @@ class Solution {
   static const int MOD = 1e9 + 7;
 
   int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
+    return DP(locations, start, finish, fuel);
+  }
+
+  int dfs(vector<int>& locations, int start, int finish, int fuel) {
     int size = locations.size();
     dp_ = vector<vector<int>> (size, vector<int>(fuel + 1, -1));
     return dfs(locations, start, finish, fuel);
   }
 
-  int dfs(vector<int>& locations, int start, int finish, int fuel) {
+  int dfsHelper(vector<int>& locations, int start, int finish, int fuel) {
     if (dp_[start][fuel] != -1) return dp_[start][fuel];
 
-    // edge condition
     int need = abs(locations[start] - locations[finish]);
     if (need > fuel) {
       dp_[start][fuel] = 0;
@@ -30,7 +33,7 @@ class Solution {
       if (i != start) {
         int need = abs(locations[start] - locations[i]);
         if (need > fuel) continue;
-        sum += dfs(locations, i, finish, fuel - need);
+        sum += dfsHelper(locations, i, finish, fuel - need);
         sum %= MOD;
       }
     }
@@ -39,4 +42,27 @@ class Solution {
   }
 
   vector<vector<int>> dp_;
+
+
+  int DP(vector<int>& locations, int start, int finish, int fuel) {
+    int n = locations.size();
+    vector<vector<int>> dp(n, vector<int>(fuel + 1, 0));
+
+    for (int k = 0; k <= fuel; k++) dp[finish][k] = 1;
+
+    for (int k = 0; k <= fuel; k++) {
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          if (j != i) {
+            int need = abs(locations[j] - locations[i]);
+            if (need > k) continue;
+            dp[i][k] += dp[j][k - need];
+            dp[i][k] %= MOD;
+          }
+        }
+      }
+    }
+
+    return dp[start][fuel];
+  }
 };
