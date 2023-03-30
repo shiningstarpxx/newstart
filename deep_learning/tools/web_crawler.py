aad2b531357df60f7774ec64e8abee734bd514d9
pei xingxin <xingxinpei@gmail.com>
2023-03-30 14:23:39 +0800

    do it with limu

---
+++ deep_learning/tools/web_crawler.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from BeautifulSoup import BeautifulSoup
+import panda as pd
+
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+
+# wirte a funtion support web crawler
